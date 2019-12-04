d4d7979a458413d7c6394ca5e71c74a1f1815c7c
Tim Taylor <tjtnew@gmail.com>
2019-12-04 12:35:59 +0000

    update based on Adam Gruer's digits function and rle use

--- day04/day04.R
+++ day04/day04.R
@@ -1,85 +1,56 @@
+# This version is heavily inspired by Adam Gruer's
+# (https://github.com/adam-gruer/aoc2019/blob/master/04.R)
+# In particular it borrows two functions he used.  Firstly the digits function
+# to convert an integer in to it's individual digits.  Secondly the 
+# Run Length Encoding function (rle) that I'd not even heard of until I saw it
+# in his solution.
+
 # input -------------------------------------------------------------------
 
 # integers to be tested
 range = 183564:657474
 
 
-# part one ----------------------------------------------------------------
+# function definitions ----------------------------------------------------
 
-# function to determine valid integer in part one
-is_valid_part_one <- function(x) {
-    
-    # convert to character
-    x <- as.character(x)
-    
-    # pull out individual digits
-    a <- substr(x, 1, 1)
-    b <- substr(x, 2, 2)
-    c <- substr(x, 3, 3)
-    d <- substr(x, 4, 4)
-    e <- substr(x, 5, 5)
-    f <- substr(x, 6, 6)
-    
-    # check ordering
-    if ((a <= b) && (b <= c) && (c <= d) && (d <= e) && (e <= f)) {
-        
-        # check for atleat one pair
-        if (anyDuplicated(c(a, b, c, d, e, f))) {
-            return(TRUE)
-        } else {
-            return(FALSE)
-        }
-    } else {
-        return(FALSE)
-    }
+# integer division then modulo division by 10 on result
+digits <- function(number, units){ number %/% units %% 10}
+
+# function to check if vector monotonically increasing
+# from https://stackoverflow.com/a/13094801/10746205
+is_increasing <- function(x) {
+    all(x == cummax(x))
 }
 
-# indices of valid integers
-tmp = lapply(range, is_valid_part_one)
+# function to determine valid vector of integers
+is_valid_part_one <- function(x) {
+    is_increasing(x) && anyDuplicated(x) > 0
+}
 
-# valid integers
-valid_integers_part_one <- range[unlist(tmp)]
+# function to determine valid integer vectors in part two.  Assumes integer
+# vectors have already been filtered in part one
+is_valid_part_two <- function(x) {
+    any(rle(x)$length == 2)
+}
+
+
+# part one ----------------------------------------------------------------
+
+# convert to digits
+range_digits <- lapply(range, digits, units = 10^c(5:0))
+
+# valid integers (logical)
+tmp = lapply(range_digits, is_valid_part_one)
+
+# valid integer vectors
+valid_integers_part_one <- range_digits[unlist(tmp)]
 
 # How many are valid in part one
 length(valid_integers_part_one)
 
-
-
 # part two ----------------------------------------------------------------
 
-# function to determine valid integer in part two.  This assumes integers have
-# already been filtered from part one
-is_valid_part_two <- function(x) {
-    
-    # convert to character
-    x <- as.character(x)
-    
-    # pull out individual digits
-    a <- substr(x, 1, 1)
-    b <- substr(x, 2, 2)
-    c <- substr(x, 3, 3)
-    d <- substr(x, 4, 4)
-    e <- substr(x, 5, 5)
-    f <- substr(x, 6, 6)
-    
-    # generate all of the integer groupings in x
-    digits <- c(a, b, c, d, e, f)
-    groups <- vector("list", 6) # as up to 6 groups possible
-    idx = 1
-    for (i in 1:5) {
-        groups[[idx]] = c(groups[[idx]], digits[i])
-        if (digits[i] != digits[i + 1]) {
-            idx = idx + 1
-        }
-    }
-    groups[[idx]] = c(groups[[idx]], digits[6])
-    
-    # Are any groups of length 2
-    #groups <- groups[lapply(groups, length) > 1]
-    any(lapply(groups, length) == 2)
-}
-
-# indices of valid integers
+# valid integers (logical)
 tmp <- lapply(valid_integers_part_one, is_valid_part_two)
 
 # valid integers
