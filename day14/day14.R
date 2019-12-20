5db831fb6bb7aff733658edcb7f2dc6acc9e587f
Tim Taylor <tjtnew@gmail.com>
2019-12-20 09:23:41 +0000

    completed day 14!

--- day14/day14.R
+++ day14/day14.R
@@ -32,25 +32,60 @@ ore_produced <- function(x, required, remainder, ingredients, output) {
         remainder[x] <- 0
     }
     
-    produced <- 0
-    ore <- 0
-    
     # need to improve this, should be able to work out how many times to run not loop
-    while(produced < required) {
-        produced <- produced + output[[x]]
-        tmp <- ingredients[[x]][[1]]
-        if (length(tmp) == 1 && tmp == "ORE") {
-            ore <- ore + ingredients[[x]][[2]]
-        } else {
-            for (i in seq_along(ingredients[[x]][[1]])) {
-                tmp <- ore_produced(ingredients[[x]][[1]][i], ingredients[[x]][[2]][i], remainder, ingredients, output)
-                ore <- ore + tmp$ore
-                remainder <- tmp$remainder
-            }
+    reactions <- ceiling(required / output[[x]])
+    produced <- reactions * output[[x]]
+    tmp <- ingredients[[x]][[1]]
+    if (length(tmp) == 1 && tmp == "ORE") {
+        ore <- reactions * ingredients[[x]][[2]]
+    } else {
+        ore <- 0
+        for (i in seq_along(ingredients[[x]][[1]])) {
+            tmp <- ore_produced(ingredients[[x]][[1]][i], reactions * ingredients[[x]][[2]][i], remainder, ingredients, output)
+            ore <- ore + tmp$ore
+            remainder <- tmp$remainder
         }
     }
+    
     remainder[x] <- produced - required
     return(list(ore = ore, remainder = remainder))
 }
 
-ore_produced("FUEL", 1, remainder, ingredients, output)$ore
+
+# part one answer ---------------------------------------------------------
+result <- ore_produced("FUEL", 1, remainder, ingredients, output)$ore
+result
+
+
+# part two ----------------------------------------------------------------
+cargo_capacity <- 1000000000000
+
+# establish higher and lower bound for binary search
+init <- cargo_capacity / result
+cargo_capacity > ore_produced("FUEL", init, remainder, ingredients, output)$ore
+cargo_capacity > ore_produced("FUEL", 2*init, remainder, ingredients, output)$ore
+
+# set bounds
+low <- init
+high <- 2*init
+
+# binary search
+while(low < high) {
+    mid <- floor((low + high) / 2)
+    tmp <- ore_produced("FUEL", mid, remainder, ingredients, output)$ore
+    if (tmp < cargo_capacity) {
+        low <- mid
+    } else if(tmp > cargo_capacity) {
+        high <- mid
+    } else if (tmp == cargo_capacity) {
+        print("found exact solution")
+    }
+    
+    if (high - low == 1) {
+        break
+    }
+}
+
+# answer part two
+low
+
